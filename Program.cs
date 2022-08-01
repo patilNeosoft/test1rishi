e90d3f7442a3f82cf49bdfb75b6411d54694add6
BE A_28_Patil Pooja <pooja777in@gmail.com>
2022-08-01 15:48:11 +0530

    file1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter product Id");
+int productId = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter product name");
+string productName = Console.ReadLine();
+
+Console.WriteLine("Enter product description");
+string productDescription = Console.ReadLine();
+
+Console.WriteLine($"ProductId : {productId}\t ProductName : {productName}\t ProductDescription : {productDescription}\t");
+
