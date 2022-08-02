5e4c5c2c0f74b543cd3740f08de74e77627cb7c2
BE A_28_Patil Pooja <pooja777in@gmail.com>
2022-08-02 14:22:13 +0530

    File1

---
+++ details/Details.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace details
+{
+    struct Details 
+    {
+        public string color;
+        public int size;
+        public int weight;
+
+        public Details(string color, int size, int weight) { 
+        this.color = color;
+        this.size = size;
+        this.weight = weight;
+
+        }
+          
+            
+            }
+}
---
+++ details/Program.cs
@@ -0,0 +1,8 @@
+﻿using details;
+product product = new product();
+Details details = new Details("black",50,5);
+product.name = "laptop";
+product.id = 100;
+product.details = details;
+
+Console.WriteLine($"product id : {product.id}\t product name : {product.name}\t product details: {product.details.color}\t{product.details.size}\t{product.details.weight}");
