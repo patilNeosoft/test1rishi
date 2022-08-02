b531d492d59e3d28e4c82105630faf5d89d852ec
BE A_28_Patil Pooja <pooja777in@gmail.com>
2022-08-02 18:48:38 +0530

    task

---
+++ methods/Program.cs
@@ -0,0 +1,10 @@
+﻿using methods.Repository;
+
+Console.WriteLine("username:");
+string userName =Console.ReadLine();
+Console.WriteLine("password:");
+string password = Console.ReadLine();
+
+repository repo = new repository();
+repo.Login(userName, password);
+
---
+++ methods/Repository/repository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods.Repository
+{
+    internal class repository
+    {
+        internal void Login(string username, string password) {
+
+            if ((username == null || username == "") && (password == null || password == ""))
+            {
+                Console.WriteLine("fill the data");
+            }
+
+            else if (username == "user1" && password == "user1")
+            {
+                Console.WriteLine("login success");
+            }
+            else {
+                Console.WriteLine("invalid");
+            }
+ 
+    }
+    }
+}
